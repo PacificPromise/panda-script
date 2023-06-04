96da8ede6449ff3cb5fe4d62bed4fa4b10abcd80
tuanngocptn <tuanngocptn@gmail.com>
2023-06-04 19:00:54 +0700

    change tag

--- index.sh
+++ index.sh
@@ -58,20 +58,30 @@ get_stage_prompt() {
     esac
   done
 }
+# create_tag() {
+#   TAG_NAME=$1
+#   echo "New release for $TAG_NAME at $(date '+%d/%m/%Y %H:%M:%S')" >>release.txt
+#   CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+#   HEAD_HASH="$(git rev-parse --short HEAD)"
+#   {
+#     git add release.txt &&
+#       git commit -m "LC-0/build: Release $TAG_NAME" &&
+#       git push origin $CURRENT_BRANCH &&
+#       git tag -a $TAG_NAME -m "New release for $TAG_NAME" &&
+#       git push origin $TAG_NAME
+#   } || {
+#     git reset --soft $HEAD_HASH
+#     git checkout $HEAD_HASH release.txt
+#   }
+# }
+
 create_tag() {
   TAG_NAME=$1
-  echo "New release for $TAG_NAME at $(date '+%d/%m/%Y %H:%M:%S')" >>release.txt
-  CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-  HEAD_HASH="$(git rev-parse --short HEAD)"
   {
-    git add release.txt &&
-      git commit -m "LC-0/build: Release $TAG_NAME" &&
-      git push origin $CURRENT_BRANCH &&
-      git tag -a $TAG_NAME -m "New release for $TAG_NAME" &&
+    git tag -a $TAG_NAME -m "New release for $TAG_NAME" &&
       git push origin $TAG_NAME
   } || {
-    git reset --soft $HEAD_HASH
-    git checkout $HEAD_HASH release.txt
+    git tag -d $TAG_NAME
   }
 }
 
